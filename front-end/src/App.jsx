1fffa144b28c7ca52252e97572d0809b555d2333
tranductinh2001 <0352911750a@gmail.com>
2024-07-08 15:16:21 +0700

    create product card

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,22 +1,19 @@
-import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import ProductDetailPage from "./pages/ProductDetailPage";
-import SearchPage from "./pages/SearchPage";
-import ContactPage from "./pages/ContactPage";
-import CartPage from "./pages/CartPage";
 import Layout from "./layout/Layout";
-import ProductPage from "./pages/ProductPage";
+import CartPage from "./pages/CartPage";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
-import UserProfilePage from "./pages/UserProfilePage";
 import NotFoundPage from "./pages/NotFoundPage";
+import ProductListPage from "./pages/ProductListPage";
+import SearchPage from "./pages/SearchPage";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
-          <Route path="products" element={<ProductPage />} />
+          <Route path="products" element={<ProductListPage />} />
           <Route path="search" element={<SearchPage />} />
           <Route path="contact" element={<ContactPage />} />
         </Route>
